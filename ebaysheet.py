15c1676de6fc14bf1ede49dc5ba295a57f6ddda2
jcr4990 <jcr4990@gmail.com>
2020-07-18 11:57:58 -0400

    update

--- ebaysheet.py
+++ ebaysheet.py
@@ -4,9 +4,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 def ebaysheet():
     scope = ['https://www.googleapis.com/auth/spreadsheets']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(r"C:\PythonScripts\ebay\Gideon-eBay-268b7a34b93b.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "gideon-ebay-fe269e8abfa1.json", scope)
     gc = gspread.authorize(creds)
     sh = gc.open_by_key("1cRZC3KRuNnYgOEyKwBmu0m9_W1BvsqhNvhdIxeXQYaQ")
     return sh
-
-
--- pyBay.py
+++ pyBay.py
@@ -4,20 +4,22 @@ from statistics import median
 import argparse
 from ebaysheet import ebaysheet
 
-# TODO: exclude arg needs to be case insensitive
+# TODO: median.etc from python script based on price not shipped price
 
 listings = []
 titles = []
 prices = []
+
 
 def get_listings(url):
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
 
     for li in soup.find_all("li", attrs={"class": "s-item"}):
-        if li.find("h3", attrs={"class":"s-item__title s-item__title--has-tags"}) is not None:
-            title = li.find("h3", attrs={"class":"s-item__title s-item__title--has-tags"}).text
-            title = title.replace("New Listing","")
+        if li.find("h3", attrs={"class": "s-item__title s-item__title--has-tags"}) is not None:
+            title = li.find(
+                "h3", attrs={"class": "s-item__title s-item__title--has-tags"}).text
+            title = title.replace("New Listing", "")
 
             if args.required is not None:
                 if args.required.lower() not in title.lower():
@@ -30,44 +32,47 @@ def get_listings(url):
                     continue
 
             titles.append(title)
-        
-        if li.find("span", attrs={"class":"s-item__price"}) is not None:
-            price = li.find("span", attrs={"class":"s-item__price"}).text
-            price = price.replace("$","").replace(",","")
-            
+
+        if li.find("span", attrs={"class": "s-item__price"}) is not None:
+            price = li.find("span", attrs={"class": "s-item__price"}).text
+            price = price.replace("$", "").replace(",", "")
+
             if "Tap item to see current price" in price:
                 continue
 
             if " to " in price:
                 price_range = price.split(" to ")
-                price_midpoint = (float(price_range[0]) + float(price_range[1])) / 2
+                price_midpoint = (
+                    float(price_range[0]) + float(price_range[1])) / 2
                 price = price_midpoint
 
             price = float(price)
             prices.append(price)
-        
-        if li.find("span", attrs={"class":"s-item__shipping s-item__logisticsCost"}) is not None:
-            shipping = li.find("span", attrs={"class":"s-item__shipping s-item__logisticsCost"}).text
-            shipping = shipping.replace("shipping","").replace("+","").strip()
-            
+
+        if li.find("span", attrs={"class": "s-item__shipping s-item__logisticsCost"}) is not None:
+            shipping = li.find(
+                "span", attrs={"class": "s-item__shipping s-item__logisticsCost"}).text
+            shipping = shipping.replace(
+                "shipping", "").replace("+", "").strip()
+
             if shipping == "Shipping not specified" or shipping == "Freight":
                 continue
 
             if shipping != "Free":
-                shipping = shipping.replace("$","")
+                shipping = shipping.replace("$", "")
                 shipped_price = price + float(shipping)
                 shipped_price = round(shipped_price, 2)
                 shipping = "$" + shipping
             elif shipping == "Free":
                 shipped_price = price
 
-        if li.find("img", attrs={"class":"s-item__image-img"}) is not None:
-            img_elem = li.find("img", attrs={"class":"s-item__image-img"})
+        if li.find("img", attrs={"class": "s-item__image-img"}) is not None:
+            img_elem = li.find("img", attrs={"class": "s-item__image-img"})
             img_url = img_elem.attrs["src"]
 
-
             # listings.append([title, "$" + str(price), shipping, "$" + str(shipped_price), f'=IMAGE("{img_url}")'])
-            listings.append([f'=IMAGE("{img_url}")', title, "$" + str(price), shipping, "$" + str(shipped_price)])
+            listings.append(
+                [f'=IMAGE("{img_url}")', title, "$" + str(price), shipping, "$" + str(shipped_price)])
 
     return listings, titles, prices
 
@@ -82,11 +87,13 @@ args = parser.parse_args()
 keywords = args.keywords
 if keywords == None:
     keywords = input("Search:")
-keywords = keywords.replace(" ","%20")
-headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+keywords = keywords.replace(" ", "%20")
+headers = {
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
 
 for page in range(1):
-    url = f"https://www.ebay.com/sch/i.html?_from=R40&_nkw={keywords}&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1&_ipg=200&_pgn={page}" # &_pgn=1
+    # &_pgn=1
+    url = f"https://www.ebay.com/sch/i.html?_from=R40&_nkw={keywords}&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1&_ipg=200&_pgn={page}"
     get_listings(url)
 
 # Debug/Test Prints
@@ -99,5 +106,5 @@ print(f"Min:{min(prices)}\nMax:{max(prices)}\nMedian:{median(prices)}")
 sh = ebaysheet()
 sh.values_clear("Sheet1!A2:M")
 
-sh.values_update('Sheet1!A2', params={'valueInputOption': 'USER_ENTERED'}, body={'values': listings}) # ValueInputOption = RAW
-
+sh.values_update('Sheet1!A2', params={'valueInputOption': 'USER_ENTERED'}, body={
+                 'values': listings})  # ValueInputOption = RAW
